9f40ef013122a0bae3309e1f0566516954a9254b
cerso <vicecers@gmail.com>
2020-09-03 19:04:17 -0300

    inicial_03_09

--- src/agenda7mo/Agenda7mo.java
+++ src/agenda7mo/Agenda7mo.java
@@ -5,7 +5,7 @@
  */
 package agenda7mo;
 
-import java.util.Enumeration;
+import java.util.*;
 
 /**
  *
@@ -17,47 +17,73 @@ public class Agenda7mo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //leer del teclado
+        int opc;
         Agenda ag = new Agenda();
         Persona p = null;
-        String d;
+        String nom, dni;
+        long tel;
+        do {
+            System.out.println("1. Agregar persona");
+            System.out.println("2. Buscar persona");
+            System.out.println("3. Eliminar persona");
+            System.out.println("4. Mostrar todas personas");
+            System.out.println("5. Salir");
 
-        if (ag.Agregar("123", "Messi", 125893)) {
-            System.out.println("Agregado");
-        } else {
-            System.out.println("Clave repetida");
-        }
+            opc = sc.nextInt();
 
-        if (ag.Agregar("133", "Ronaldo", 126587)) {
-            System.out.println("Agregado");
-        } else {
-            System.out.println("Clave repetida");
-        }
+            switch (opc) {
+                case 1:
+                    System.out.println("Ingrese el nombre: ");
+                    nom = sc.next();
+                    System.out.println("Ingrese el DNI: ");
+                    dni = sc.next();
+                    System.out.println("Ingrese el telefono: ");
+                    tel = sc.nextLong();
 
-        if (ag.Eliminar("1234")) {
-            System.out.println("Eliminado");
-        } else {
-            System.out.println("No existe");
-        }
-        
-        p = ag.Recuperar("123");
+                    if (ag.Agregar(dni, nom, tel)) {
+                        System.out.println("La persona se agrego correctamente");
+                    } else {
+                        System.out.println("La persona no se agrego DNI repetido");
+                    }
 
-        if (p != null) {
-            System.out.println(p.toString());
-        } else {
-            System.out.println("No exixte el dni");
-        }
-        
-        Enumeration<String> e = ag.Total();
-        
-        while(e.hasMoreElements()){
-            d=e.nextElement();
-            Persona per = ag.Recuperar(d);
-            System.out.println(per.toString());
-        }
-        
-        
+                    break;
+                case 2:
+                    System.out.println("Ingrese el DNI: ");
+                    dni = sc.next();
+                    p = ag.Recuperar(dni);
+
+                    if (p != null) {
+                        System.out.println(p.getNombre());
+                        System.out.println(p.getDni());
+                        System.out.println(p.getTel());
+                        System.out.println(p.toString());
+                    } else {
+                        System.out.println("El DNI no existe");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Ingrese el DNI: ");
+                    dni = sc.next();
+                    if (ag.Eliminar(dni)) {
+                        System.out.println("La persona h sido eliminada");
+                    } else {
+                        System.out.println("El DNI no existe");
+                    }
+                    break;
+                case 4:
+                    Enumeration<String> e = ag.Total();
+                    while (e.hasMoreElements()) {
+                        String d = e.nextElement();
+                        Persona per = ag.Recuperar(d);
+                        System.out.println(per.toString());
+                    }
+
+                    break;
+            }
+
+        } while (opc != 5);
+
     }
-    
-    
 
 }
